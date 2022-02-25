1022e9189e1be19afb60f737db895e16cd4f9595
gsomer <gasomer75@hotmail.com>
2022-02-24 23:44:30 -0800

    add Player constructor and tests

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -1,16 +1,19 @@
 const Potion = require('../lib/Potion.js');
 
-test('creates a health potion object', () => {
-  const potion = new Potion('health');
+describe('Potion', () => {
 
-  expect(potion.name).toBe('health');
-  expect(potion.value).toEqual(expect.any(Number));
-});
+  it('creates a health potion object', () => {
+    const potion = new Potion('health');
 
-test('creates a random potion object', () => {
-  const potion = new Potion();
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
 
-  expect(potion.name).toEqual(expect.any(String));
-  expect(potion.name.length).toBeGreaterThan(0);
-  expect(potion.value).toEqual(expect.any(Number));
+  it('creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
 });
